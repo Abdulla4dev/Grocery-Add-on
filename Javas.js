df1a312ef3796f3b9a7da7890868fcca0531fdf9
Abdulla4dev <71752696+Abdulla4dev@users.noreply.github.com>
2022-06-04 16:20:14 +0530

    Add files via upload

---
+++ Javas.js
@@ -0,0 +1,22 @@
+const alert = document.querySelector('.alert') ;
+const form = document.querySelector('.grocery-form');
+const grocery = document.getElementById('grocery');
+const submitBtn = document.querySelector('.submit-btn');
+const container = document.querySelector('.grocery-container');
+const list = document.querySelector('.grocery-list');
+const clearBtn = document.querySelector('.clear-btn');
+
+//edit
+let editElement ;
+let editFlag = false;
+let editID = "";
+
+ //**listerners
+//Submit
+form.addEventListener('submit',addItem)
+//functiom
+function addItem(e){
+    e.preventDefault();
+    console.log("item")
+}
+
